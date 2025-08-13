69b6ef4e8811a136ae74d5765f4e32aaba18197c
NotSwanand <145210290+NotSwanand@users.noreply.github.com>
2025-08-13 13:08:39 +0530

    Create Test if tuple is distinct.py

---
+++ Test if tuple is distinct.py
@@ -0,0 +1,23 @@
+# Given a tuple arr , print "True" if all elements of tuple are different otherwise print "False".
+
+# A tuple is a collection of items that are ordered and unchangeable.
+
+# Examples:
+
+# Input:
+# arr = (1, 2, 3, 4, 5, 4)
+# Output: False
+# Input:
+# arr = (1, 2, 3, 4, 5)
+# Output: True
+
+#User function Template for python3
+arr = tuple(map(int, input().split()))
+
+########### Write your code below ###############
+# Print "True" if all elements of tuple are different, otherwise print "False"
+if len(arr) == len(set(arr)):
+    print("True")
+else:
+    print("False")
+########### Write your code above ###############
