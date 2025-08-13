253bfb27d9326777011d1e43ca18e1c51b689d79
NotSwanand <145210290+NotSwanand@users.noreply.github.com>
2025-08-13 13:15:16 +0530

    Create TypeCast And Double It.py

---
+++ TypeCast And Double It.py
@@ -0,0 +1,16 @@
+# Given an input num as a string. You need to typecast into an integer and double it. 
+
+# Examples:
+
+# Input: num = "5"
+# Output: 10
+# Explanation: Typecast "5" to int and then double it 5 * 2 = 10
+# Input: num = "12"
+# Output: 24
+# Explanation: Typecast "12" to int and then double it 12 * 2 = 24
+# Constraints:
+# 0 <= num <= 105
+
+num = input()
+#code here
+print(int(num)*2)
