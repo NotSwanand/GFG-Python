93ed5af5810fe0c19e552f0c05d05516e6436584
NotSwanand <145210290+NotSwanand@users.noreply.github.com>
2025-08-14 22:55:11 +0530

    Create Arithmetic Operators.py

---
+++ Introducing Python/Arithmetic Operators.py
@@ -0,0 +1,33 @@
+# You are given two integer variables x and y. You need to perform the following operations:
+
+# p = x + y : Addition
+# q = x - y : Subtraction
+# r = x * y :Multiplication
+# s = x / y : Division
+# t = x % y : Modulo
+# Examples:
+
+# Input: x = 1, y = 2
+# Output: 3 -1 2 0 1 
+# Explanation: The given operations are performed.
+# Input: x = 3, y = 4 
+# Output: 7 -1 12 0 3 
+# Explanation: The given operations are performed
+# Constraints:
+# -100 ≤ x ≤ 100
+# -100 ≤ y ≤ 100
+
+x=int(input())
+y=int(input())
+#code here
+# Perform addition x+y below
+p = x+y
+# Perform subtraction x-y below
+q = x-y
+# Perform multiplication x*y below
+r = x*y
+# Perform integer divison x//y below
+s = x//y
+# Perform modulo operation x%y below
+t = x % y
+print(p, q, r, s, t)
