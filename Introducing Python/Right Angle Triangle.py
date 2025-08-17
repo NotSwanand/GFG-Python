4ee4983113fa652741ebefb10b349b5d59b1e27c
NotSwanand <145210290+NotSwanand@users.noreply.github.com>
2025-08-17 23:54:45 +0530

    Create Right Angle Triangle.py

---
+++ Introducing Python/Right Angle Triangle.py
@@ -0,0 +1,19 @@
+Given an integer n. Write a program to print the Right angle triangle wall. The length of perpendicular and base is n.  Use single loop and string multiplication.
+
+Note: Print exactly single " " after "*". Print a new line after printing the triangle.
+
+Examples:
+
+Input: n = 4
+Output:
+* 
+* * 
+* * * 
+* * * * 
+Explanation: Length of perpendicular and base of triangle is 4 .
+Input: n = 3
+Output:
+* 
+* * 
+* * * 
+Explanation: Length of perpendicular and base of triangle is 3 .
