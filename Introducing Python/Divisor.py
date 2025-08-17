327dadde0faea4efcbb65517d7ef390a77d6de62
NotSwanand <145210290+NotSwanand@users.noreply.github.com>
2025-08-17 23:57:55 +0530

    Create Divisor.py

---
+++ Introducing Python/Divisor.py
@@ -0,0 +1,12 @@
+Given an integer n. Write a program to print all the divisors of n in a single line.
+
+Examples:
+
+Input: n = 12
+Output: 1 2 3 4 6 12
+Explanation: 12 is divisible by 1 2 3 4 6 12.
+Input: n = 10
+Output: 1 2 5 10
+Explanation: 10 is divisible by 1 2 5 10.
+Constraints:
+1 <= n <= 105
