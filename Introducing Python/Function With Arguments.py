816c985ce730def49de4f1173d4956eaae53a5b4
NotSwanand <145210290+NotSwanand@users.noreply.github.com>
2025-08-21 23:03:16 +0530

    Create Function With Arguments.py

---
+++ Introducing Python/Function With Arguments.py
@@ -0,0 +1,7 @@
+# Here two integers a and b are given. The given input and its values are passed as arguments to the function argumentFunction. The argumentFunction is responsible to return (a+b). You need to write this function.
+
+# Examples:
+
+# Input: a = 2, b = 3
+# Output: 5
+# Explanation: 2 + 3 = 5
