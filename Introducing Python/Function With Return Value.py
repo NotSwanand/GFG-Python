bc955128f4e21f21a34636b455a632cba76f5734
NotSwanand <145210290+NotSwanand@users.noreply.github.com>
2025-08-23 15:28:38 +0530

    Update Function With Return Value.py

--- Introducing Python/Function With Return Value.py
+++ Introducing Python/Function With Return Value.py
@@ -1,12 +1,11 @@
 
 
-# Here one integer n is given. You need to write the complete function returnValueFunction that takes n as a parameter and uses the return keyword to return double the value of n.
+# # Here one integer n is given. You need to write the complete function returnValueFunction that takes n as a parameter and uses the return keyword to return double the value of n.
 
-# Examples:
+# # Examples:
 
-# Input: n = 2
-# Output: 4
-# Explanation: 2 * 2 = 4
+# # Input: n = 2
+# # Output: 4
+# # Explanation: 2 * 2 = 4
 # Constraints:
 # 1 ≤ n ≤ 5
-
