de90bc2512c0aca90aeb832b792bdccca79aab36
NotSwanand <145210290+NotSwanand@users.noreply.github.com>
2025-08-27 09:53:34 +0530

    Update Function With Arguments.py

--- Introducing Python/Functions/Function With Arguments.py
+++ Introducing Python/Functions/Function With Arguments.py
@@ -5,3 +5,13 @@
 # Input: a = 2, b = 3
 # Output: 5
 # Explanation: 2 + 3 = 5
+
+#User function Template for python3
+
+#Write the complete argumentFunction below.
+#The function should take two arguments a and b
+#The function should return a+b
+#code here
+def argumentFunction(a,b):
+    return a+b
+#Write the argumentFunction above.
