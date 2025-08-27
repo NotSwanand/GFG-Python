0e5f228221a0c208f5ff1163ef29e093276a08d9
NotSwanand <145210290+NotSwanand@users.noreply.github.com>
2025-08-27 09:55:15 +0530

    Update Prime Factorization.py

--- Introducing Python/Functions/Prime Factorization.py
+++ Introducing Python/Functions/Prime Factorization.py
@@ -12,3 +12,21 @@
 # Explanation: 27 = 3 * 3 * 3 
 # Constraint:
 # 2 <= n <= 200
+
+class Solution:
+    def printPrimeFactorization(self, n):
+        #code here
+        while n % 2 == 0:
+            print(2, end=" ")
+            n //= 2
+            
+        i = 3
+        while i*i <= n:
+            while n % i == 0:
+                print(i, end= " ")
+                n //= i
+            i+=2
+            
+        if n > 2:
+            print(n, end=" ")
+                
