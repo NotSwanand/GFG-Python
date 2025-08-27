c697fa07c2180a1814e3e821bac1db8363329a04
NotSwanand <145210290+NotSwanand@users.noreply.github.com>
2025-08-27 09:53:57 +0530

    Update Function With No Arguments.py

--- Introducing Python/Functions/Function With No Arguments.py
+++ Introducing Python/Functions/Function With No Arguments.py
@@ -5,3 +5,12 @@
 # Input: None
 # Output: Hello
 # Explanation: There is no input. The function prints Hello everytime.
+
+#User function Template for python3
+
+#Write the helloFunction below. It should have one statement
+#print("Hello")
+#code here
+def helloFunction():
+    print("Hello")
+    
