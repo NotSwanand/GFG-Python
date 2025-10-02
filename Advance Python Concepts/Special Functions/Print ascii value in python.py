15788f70ff1e00f695d54967202d8b2335ac9c0f
Swanand Bowalekar <145210290+NotSwanand@users.noreply.github.com>
2025-10-03 00:38:27 +0530

    Create Print ascii value in python.py

---
+++ Advance Python Concepts/Special Functions/Print ascii value in python.py
@@ -0,0 +1,13 @@
+# Given a character ch as input, print its ascii value.
+
+# Examples:
+
+# Input: ch = 'a'
+# Output: 97
+# Input: ch = 'A'
+# Output: 65
+#User function Template for python3
+class Solution:
+    def printAscii(self, ch):
+        ascii = ord(ch)
+        print(ascii)
